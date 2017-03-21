e020ff21652ba862a933c978994bbfc3224ac130
visionarylabs <visionarylabs@gmail.com>
2017-03-22 04:00:37 +0800

    test2

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ require(__dirname + '/app.js');
 test('http response', function (t) {
 	require('http').get("http://localhost:8080", function (res) {
 		res.on('data', function (chunk) {
-			t.equal(chunk.toString(), "Hello World");
+			t.equal(chunk.toString(), "Hello World - Docker Build");
 			t.end();
 			process.exit(0);
 		});
